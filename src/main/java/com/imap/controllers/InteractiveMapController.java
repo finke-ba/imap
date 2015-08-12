1a788cbddafa27f2920501af8ad39f7663e18cd9
madkroll <mighty.boosta@gmail.com>
2015-08-12 23:06:14 +0300

    Enabled sending JSON objects as response

--- src/main/java/com/imap/controllers/InteractiveMapController.java
+++ src/main/java/com/imap/controllers/InteractiveMapController.java
@@ -1,18 +1,18 @@
 package com.imap.controllers;
 
 import com.imap.dto.ResponseBoiler;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by Boris Finkelshtein.
  */
 @RestController
-//@RequestMapping("/imap")
+@RequestMapping("/imap")
 public class InteractiveMapController {
 
-	@RequestMapping("/boiler")
+	@RequestMapping(value = "/boiler", method = RequestMethod.GET)
 	public ResponseBoiler getBoiler() {
 		return new ResponseBoiler("Boiler");
 	}
