b4499b356fd2aaddfa99f7594a5e576910ac228b
finke.ba <finke.ba@gmail.com>
2016-11-07 12:18:03 +0300

    Added two tables to h2.

--- src/main/resources/db/sql/create-db.sql
+++ src/main/resources/db/sql/create-db.sql
@@ -11,3 +11,18 @@ CREATE TABLE boiler (
   address  VARCHAR(64),
   tel      VARCHAR(16)
 );
+
+CREATE TABLE sensors (
+  id_co     INT(10),
+  boiler_id INT(6) REFERENCES boiler(id)
+);
+
+CREATE TABLE actual_param_values (
+  id_pd     INT(6),
+  par_value VARCHAR(16),
+  date_time DATETIME,
+  id_co     INT(10),
+  par_name  VARCHAR(16),
+  par_dim   VARCHAR(8),
+  par_num   INT(4)
+);
--- src/main/resources/db/sql/insert-data.sql
+++ src/main/resources/db/sql/insert-data.sql
@@ -1,3 +1,7 @@
 INSERT INTO towns VALUES (1, 'Rostov-on-Don', 'Ростов-на-Дону');
 
 INSERT INTO boiler VALUES (1, 'Котельная1', 1, 'Садовая 25', '2456595');
+
+INSERT INTO sensors VALUES (1, 1);
+
+INSERT INTO actual_param_values VALUES (1, '123', curdate(), 1, 'T01', 'T', 1);
