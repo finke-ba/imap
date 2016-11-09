2fa4e7cbfb35c8c6f669e0adf4cdf153790e052a
finke.ba <finke.ba@gmail.com>
2016-11-09 10:23:22 +0300

    Added full db create and insert scripts.

--- src/main/resources/db/sql/insert-data.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO towns VALUES (1, 'Rostov-on-Don', 'Ростов-на-Дону');
-
-INSERT INTO boiler VALUES (1, 'Котельная1', 1, 'Садовая 25', '2456595');
-
-INSERT INTO sensors VALUES (1, 1);
-
-INSERT INTO actual_param_values VALUES (1, '123', curdate(), 1, 'T01', 'T', 1);
