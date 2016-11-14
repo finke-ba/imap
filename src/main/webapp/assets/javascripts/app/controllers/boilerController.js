32ee8170ba9fa942c3d8edd2e68986889eb94b22
finke.ba <finke.ba@gmail.com>
2016-11-14 11:05:50 +0300

    Implemented boiler view and controller.

---
+++ src/main/webapp/assets/javascripts/app/controllers/boilerController.js
@@ -0,0 +1,32 @@
+angular.module("imap").controller("boilerController", function ($scope, $http, $location) {
+
+  $scope.boilerId = $location.search().boilerId;
+  $scope.controlObjectsInBoiler = [];
+
+  getBoilerChecked();
+
+  function getBoilerChecked() {
+    $http.get('/imap/boiler/'+ $scope.boilerId + "/check").then(function (response) {
+      $scope.controlObjectsInBoiler = response.data;
+    });
+  }
+
+  $scope.getStatusClass= function(paramStatusId) {
+    if(paramStatusId == 1) {
+      return "success";
+    }
+    else if(paramStatusId == 2) {
+      return "warning";
+    }
+    else if(paramStatusId == 3) {
+      return "danger";
+    }
+  };
+
+  $scope.formatDate = function(date){
+    var date = date.split("-").join("/");
+    var dateOut = new Date(date);
+    return dateOut;
+  };
+
+});
